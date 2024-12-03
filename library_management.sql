e54b8dcb7976f4a05f811c19b045414eea4560a9
DhutaPanji <dhutapanji50@gmail.com>
2024-12-03 20:41:36 +0700

    first commit

---
+++ library_management.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Des 2024 pada 14.35
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_buku`
+--
+
+CREATE TABLE `tb_buku` (
+  `buku_id` int(11) NOT NULL,
+  `judulBUKU` varchar(100) NOT NULL,
+  `penulis` varchar(100) NOT NULL,
+  `tahun_publikasi` year(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_buku`
+--
+
+INSERT INTO `tb_buku` (`buku_id`, `judulBUKU`, `penulis`, `tahun_publikasi`) VALUES
+(1, 'tukang seblak ketabrak lori', 'imam', '2024'),
+(3, 'aku sehat walafiat', 'panji', '2024'),
+(5, 'hidup sehat', 'iwiw', '1945');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  ADD PRIMARY KEY (`buku_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  MODIFY `buku_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
